aff466388b577d1899d446ab44214323ef1a799b
s.turaev <suhrobturaev2004@gmail.com>
2025-11-25 11:08:16 +0500

    feat full

--- core/settings.py
+++ core/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'django_celery_beat',
 ]
 
 MIDDLEWARE = [
@@ -73,11 +75,16 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "tg_db",
+        "USER": "postgres",
+        "PASSWORD": "qwerty",
+        "HOST": "db",
+        "PORT": 5432,
     }
 }
+
 
 
 # Password validation
